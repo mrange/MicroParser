2d7ae0932bcbcaae92d50a8e1f9fa60e96611b1d
marten_range <2491891+mrange@users.noreply.github.com>
2010-07-05 23:13:53 +0200

    Added Performance tracer project.

--- MicroParser/Extensions.cs
+++ MicroParser/Extensions.cs
@@ -39,18 +39,5 @@ namespace MicroParser
          sb.Append (append ?? "");
          return sb.ToString ();
       }
-
-      public static TValue Lookup<TKey, TValue> (
-         this IDictionary<TKey, TValue> dictionary,
-         TKey key,
-         TValue defaultValue
-         )
-      {
-         TValue value;
-         return dictionary.TryGetValue (key, out value)
-                   ? value
-                   : defaultValue;
-      }
-
    }
 }
