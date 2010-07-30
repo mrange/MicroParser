a7eeb29f98ee030fbbc636bf85b93583ee4d5808
marten_range <marten_range@localhost>
2010-07-30 09:02:16 +0200

    Updated project includes

--- Bindings/IncludeProject.cs
+++ Bindings/IncludeProject.cs
@@ -1198,78 +1198,6 @@ namespace MicroParser
                    };
       }
 
-      public static ParserFunction<Tuple<TValue1, TValue2>> Group<TValue1, TValue2> (
-         ParserFunction<TValue1> firstParser,
-         ParserFunction<TValue2> secondParser
-         )
-      {
-         return state =>
-         {
-            var initialPosition = state.Position;
-
-            var firstResult = firstParser (state);
-
-            if (firstResult.State.HasError ())
-            {
-               return firstResult.Failure<Tuple<TValue1, TValue2>> ();
-            }
-
-            var secondResult = secondParser (state);
-
-            if (secondResult.State.HasError ())
-            {
-               return secondResult.Failure<Tuple<TValue1, TValue2>> ().VerifyConsistency (initialPosition);
-            }
-
-            return secondResult.Success (
-               Tuple.Create (
-                  firstResult.Value,
-                  secondResult.Value
-                  ));
-         };
-      }
-
-      public static ParserFunction<Tuple<TValue1, TValue2, TValue3>> Group<TValue1, TValue2, TValue3>(
-         ParserFunction<TValue1> firstParser, 
-         ParserFunction<TValue2> secondParser,
-         ParserFunction<TValue3> thirdParser
-         )
-      {
-         return state =>
-         {
-            var initialPosition = state.Position;
-
-            var firstResult = firstParser (state);
-
-            if (firstResult.State.HasError ())
-            {
-               return firstResult.Failure<Tuple<TValue1, TValue2, TValue3>> ();
-            }
-
-            var secondResult = secondParser (state);
-
-            if (secondResult.State.HasError ())
-            {
-               return secondResult.Failure<Tuple<TValue1, TValue2, TValue3>> ().VerifyConsistency (initialPosition);
-            }
-
-            var thirdResult = thirdParser (state);
-
-            if (thirdResult.State.HasError ())
-            {
-               return thirdResult.Failure<Tuple<TValue1, TValue2, TValue3>> ().VerifyConsistency (initialPosition);
-            }
-
-
-            return thirdResult.Success (
-               Tuple.Create (
-                  firstResult.Value,
-                  secondResult.Value,
-                  thirdResult.Value
-                  ));
-         };
-      }
-
       public static ParserFunction<TValue> Attempt<TValue> (
          this ParserFunction<TValue> firstParser
          )
@@ -1603,6 +1531,94 @@ namespace MicroParser
       
    }
 }
+// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+
+namespace MicroParser
+{
+   using System;
+   using MicroParser.Internal;
+	partial class Parser
+	{
+      public static ParserFunction<Tuple<TValue1, TValue2>> Group<TValue1, TValue2> (
+            ParserFunction<TValue1> parser1
+         ,  ParserFunction<TValue2> parser2
+         )
+      {
+         return state =>
+         {
+            var initialPosition = state.Position;
+
+            var result1 = parser1 (state);
+
+            if (result1.State.HasError())
+            {
+               return result1.Failure<Tuple<TValue1, TValue2>>().VerifyConsistency(initialPosition);
+            }
+            var result2 = parser2 (state);
+
+            if (result2.State.HasError())
+            {
+               return result2.Failure<Tuple<TValue1, TValue2>>().VerifyConsistency(initialPosition);
+            }
+            return result2.Success(
+               Tuple.Create(
+                     result1.Value
+                  ,  result2.Value
+                  ));
+         };
+      }
+      public static ParserFunction<Tuple<TValue1, TValue2, TValue3>> Group<TValue1, TValue2, TValue3> (
+            ParserFunction<TValue1> parser1
+         ,  ParserFunction<TValue2> parser2
+         ,  ParserFunction<TValue3> parser3
+         )
+      {
+         return state =>
+         {
+            var initialPosition = state.Position;
+
+            var result1 = parser1 (state);
+
+            if (result1.State.HasError())
+            {
+               return result1.Failure<Tuple<TValue1, TValue2, TValue3>>().VerifyConsistency(initialPosition);
+            }
+            var result2 = parser2 (state);
+
+            if (result2.State.HasError())
+            {
+               return result2.Failure<Tuple<TValue1, TValue2, TValue3>>().VerifyConsistency(initialPosition);
+            }
+            var result3 = parser3 (state);
+
+            if (result3.State.HasError())
+            {
+               return result3.Failure<Tuple<TValue1, TValue2, TValue3>>().VerifyConsistency(initialPosition);
+            }
+            return result3.Success(
+               Tuple.Create(
+                     result1.Value
+                  ,  result2.Value
+                  ,  result3.Value
+                  ));
+         };
+      }
+
+
+   }
+}
+
 // ----------------------------------------------------------------------------------------------
 // Copyright (c) Mårten Rånge.
 // ----------------------------------------------------------------------------------------------
