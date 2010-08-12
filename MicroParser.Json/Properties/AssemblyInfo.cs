075b47f8fd638af82d38ca55c7705abb19f00009
marten_range <marten_range@localhost>
2010-08-12 11:50:12 +0200

    Added strong naming. Added global assembly info file

--- MicroParser.Json/Properties/AssemblyInfo.cs
+++ MicroParser.Json/Properties/AssemblyInfo.cs
@@ -9,41 +9,8 @@
 // ----------------------------------------------------------------------------------------------
 // You must not remove this notice, or any other, from this software.
 // ----------------------------------------------------------------------------------------------
-using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle ("MicroParse.Json")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("microparser.codeplex.com")]
-[assembly: AssemblyProduct ("MicroParser")]
-[assembly: AssemblyCopyright ("Copyright © Mårten Rånge 2010")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
-
-[assembly: CLSCompliant (true)]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible (false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid ("a88f7f89-0bb6-4a1f-8518-fb06c28720ee")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion ("1.0.*")]
-[assembly: AssemblyVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion ("1.0.0.0")]
