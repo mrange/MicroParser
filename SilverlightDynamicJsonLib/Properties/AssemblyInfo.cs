d87c2b7cb98b03bb3b5abee09bbb163a1990cd3b
mrange <2491891+mrange@users.noreply.github.com>
2011-10-02 19:15:11 +0200

    Centralized Json parser and added Windows Phone support

---
+++ SilverlightDynamicJsonLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SilverlightDynamicJsonLib")]
+[assembly: Guid("54421635-6d28-4e02-8a16-c1c8ffc50f6e")]
---
+++ WindowsPhoneJsonLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WindowsPhoneJsonLib")]
+[assembly: Guid("b6f82279-fdd2-4b15-99f7-1ddc85d53dc0")]
